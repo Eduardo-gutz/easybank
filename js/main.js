97e36f730461b071673eda453ef79f070b13cf75
Eduardo Gutz <lalo_gutak@hotmail.com>
2021-02-21 22:38:20 -0600

    easybank template

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('hamburger');
+const ham = document.querySelector('.hamburger > img');
+const menu = document.getElementById('menu');
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (menu.classList.contains('nav-bar__menu_active')) {
+    menu.classList.remove('nav-bar__menu_active');
+    btn.title = 'active menu';
+    ham.src = '../images/icon-hamburger.svg';
+  } else {
+    btn.title = 'close menu';
+    menu.classList.add('nav-bar__menu_active');
+    ham.src = '../images/icon-close.svg';
+  }
+});
